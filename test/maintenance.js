6559c8183d5cdd7cb680a57c4d2e317a7cd67d98
ychen11 <chenyiwei1987@gmail.com>
2016-08-07 01:24:54 +0800

    added maintenance

---
+++ test/maintenance.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+var clientObj = require('./a');
+
+describe('Maintenance', function() {
+  var etcdClient = clientObj.client,
+      leaderId = null;
+
+  describe('Get status', function () {
+    it ('Should return the cluster status', function (done) {
+      etcdClient.maintenance.getStatus({}, function (err, res) {
+        assert.ifError(err);
+        leaderId = res.leader;
+        done();
+      });
+    });
+
+    it ('Should return the leader id', function (done) {
+      etcdClient.maintenance.getLeaderId(function (err, res) {
+        assert.ifError(err);
+        assert.strictEqual(res, leaderId);
+        done();
+      })
+    })
+  });
+});
